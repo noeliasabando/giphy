d02aed0a2a0a11c897b766af53dd0c0f68924b82
Noelia Sabando <lolia_15@hotmail.com>
2018-06-19 00:33:37 -0400

    llamada a API lista

--- fech/assets/js/app.js
+++ fech/assets/js/app.js
@@ -2,19 +2,22 @@ const inputText= document.querySelector("input");
 const boton= document.getElementById("buscar-gif");
 const containerImage= document.getElementById("img")
 
-inputText.addEventListener("click", event=>{
-  let gif= document.getElementById("gif-text").value="";
-    console.log(gif)
-})
-
-fetch(`https://api.giphy.com/v1/gifs/trending?api_key=qGfs8n75RXQjRDXfiAZYistSGYAmey7O&limit=25&rating=G`)
-  .then(respgif => respgif.json())
+boton.addEventListener("click", event=>{
+  let gif= document.getElementById("gif-text").value;
+  inputText.value="";
+  fetch(`https://api.giphy.com/v1/gifs/search?api_key=qGfs8n75RXQjRDXfiAZYistSGYAmey7O&q=${gif}&limit=25&offset=0&rating=G&lang=es`)
+  .then(response => response.json())
   .then(datos =>{
     console.log(datos);
     renderInfo(datos);
   })
+})
 
-  const renderInfo= datos =>{
-    containerImage.innerHTML += `<img src="${datos.url}">`;
-  }
+
+const renderInfo= datos =>{
+  containerImage.innerHTML="";
+  datos.data.forEach((gif)=>{
+    containerImage.innerHTML += `<img src="${gif.images.fixed_width.url}">`;
+  })   
+}
   
